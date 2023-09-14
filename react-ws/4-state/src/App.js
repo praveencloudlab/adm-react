817d0eae5c0d133b9baa6a0e1c725657289625c0
Praveenreddy <praveen@Praveenreddys-MacBook-Pro.local>
2023-09-14 10:33:48 +0530

    props and state

---
+++ react-ws/4-state/src/App.js
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+import Greet from './Greet';
+
+const App = () => {
+
+   const[counter,setCounter]=useState(1); // useState always retus an array.
+   const[even,setEven] = useState(2); 
+   const[odd,setOdd] = useState(1);
+   const[greetMsg,setGreetMsg] = useState('')
+  
+   function increment(){
+    console.log(">> App:: increment");
+    setCounter(counter+1) // state is updated
+  }
+  return (
+    <div>
+     
+      <h1>App Component</h1> 
+     
+      <h3>Count: {counter}</h3>
+      <h3>Even: {even}</h3>
+      <h3>Odd: {odd}</h3>
+
+      <button onClick={increment}>Count</button>
+      <button onClick={()=>setEven(even+2)}>Increment Even</button>
+      <button onClick={()=>setOdd(odd+2)}>Increment Odd</button>
+      <button onClick={()=>setGreetMsg('Good Morning')}>Good Morning</button>
+      <button onClick={()=>setGreetMsg('Good Noon')}>Good Noon</button>
+      <button onClick={()=>setGreetMsg('Good Evening')}>Good Evening</button>
+
+      <hr/>
+
+
+      <Greet msg={greetMsg}/>
+    </div>
+  );
+};
+
+export default App;
---
+++ react-ws/4-state/src/Greet.js
@@ -0,0 +1,15 @@
+const Greet = ({msg}) => {
+
+   // let msg=props.msg;
+
+    return (
+        <div>
+
+        <h1>Greet Component</h1>
+        <h3>message: {msg}</h3>
+            
+        </div>
+    );
+};
+
+export default Greet;
